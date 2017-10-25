ed0c99f0cefc935a93bd77b8d6c8fabed9b44244
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-10-24 22:01:20 -0500

    Enabling nabar close on clicking outside the navbar

--- js/transformation.js
+++ js/transformation.js
@@ -4,65 +4,18 @@ if (window.attachEvent) //if IE (and Opera depending on user setting)
     window.attachEvent("on"+mousewheelevt, function(e){scrollEvent(e)})
 else if (window.addEventListener) //WC3 browsers
     window.addEventListener(mousewheelevt, function(e){scrollEvent(e)}, false)
-
-// $(window).on('mousewheel', scrollEvent);
 $(window).on('touchstart', touchStart);
 $(window).on('touchend', touchEnd);
-$(document).ready(function(){
-  $("#show-next-page").on('click', function(e){
-    e.preventDefault();
-    go_down();
-  });
-  $("#show-step-3").on('click', function(e){
-    e.preventDefault();
-    go_down();
-  })
-});
-var url = window.location.href;
-var timout_interval = 500;
-function go_down(){
-  if($("#city-animation").is(":visible")){
-    setTimeout(function(){
-      step2();
-      document.location.hash = "step2";
-    }, timout_interval*2);
-    setTimeout(function(){}, timout_interval*2);
-  } else if($("#table-animation").is(":visible")){
-    setTimeout(function(){
-      step3();
-      document.location.hash = "step3";
-    }, timout_interval*2);
-    setTimeout(function(){}, timout_interval*2);
-  } else if($("#step3").is(":visible")){
-    step4();
-    document.location.hash = "step4";
-  }
-}
-function go_up(){
-  if($("#step4").is(":visible")){
-    setTimeout(function(){
-      step3();
-      document.location.hash = "step3";
-    }, timout_interval*2);
-  }
-  if($("#step3").is(":visible")){
-    setTimeout(function(){
-      step2();
-      document.location.hash = "step2";
-    }, timout_interval*2);
-  } else if($("#table-animation").is(":visible")){
-      step1();
-      document.location.hash = "";
-  }
-}
 function scrollEvent(e){
   var evt = window.event || e;
   var delta = evt.detail? evt.detail*(-4) : evt.wheelDelta;
-  if(delta< -50) {
-    go_down();
-  }else if(delta > 50) {
+  if(delta< -150) {
+      $(".carousel").carousel('next');
+      setTimeout(function(){}, 2000);
+  }else if(delta > 150) {
    //scroll up
-    go_up();    
+    $(".carousel").carousel('prev');
+    setTimeout(function(){}, 2000);
   }      
 }
 var ts;
@@ -72,67 +25,15 @@ function touchStart(e){
 function touchEnd(e){
   var te = e.originalEvent.changedTouches[0].clientY;
  if(ts > te+10){
-    go_down();
+    $(".carousel").carousel('next');
  }else if(ts < te-10){
-    go_up();
+    $(".carousel").carousel('prev');
  }
 }
-
-function step1(){
-  $("#table-animation").hide();
-  $("#step3").hide();
-  $("#step4").hide();
-  $("#city-animation").fadeIn(1000);
+function closeNavOnClickDocument(event){
+  if(!$(event.target).closest('#mySidenav').length && !$(event.target).closest('#hamburger').length) {
+    if($('#mySidenav').is(":visible")) {
+        closeNav();
+    }  
+  }
 }
-function step2(){
-  $("#city-animation").hide();
-  $("#step3").hide();
-  $("#step4").hide();
-  $("#table-animation").css('display', 'block');
-  $("#table-animation").css('opacity', '1');
-}
-
-function step3(){
-  $("#table-animation").hide()
-  $("#city-animation").hide();
-  $("#step4").hide();
-  $("#step3").css('display', 'block');
-  $("#step3").css('opacity', '1');
-}
-function step4(){
-  $("#table-animation").hide();
-  $("#city-animation").hide();
-  $("#step4").css('display', 'block');
-  $("#step4").css('opacity', '1');
-}
-$(document).ready(function(){
-  var baseUrl = "http://localhost:8080/";
-  var link = window.location.href;
-  window.onhashchange = function(){
-    var hash = document.location.hash;
-    if(hash === "" || hash.indexOf("#step1") >= 0){
-      step1();
-    }
-    if(hash.indexOf("#step2") >= 0){
-      step2();
-    }
-    if(hash.indexOf("#step3") >= 0){
-      step3();
-    }
-    if(hash.indexOf("#step4") >= 0){
-      step4();
-    }
-  }
-  if(link === baseUrl || link.indexOf("#step1") >= 0){
-    step1();
-  }
-  if(link.indexOf("#step2") >= 0){
-    step2();
-  }
-  if(link.indexOf("#step3") >= 0){
-    step3();
-  }
-  if(link.indexOf("#step4") >= 0){
-    step4();
-  }
-})
