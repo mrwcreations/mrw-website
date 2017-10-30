61e952db76fb8141e154d9d43527e6453918a60d
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-10-30 12:58:47 -0500

    Adding exception handling for send email module

--- scripts/mail/sendmail.php
+++ scripts/mail/sendmail.php
@@ -1,29 +1,38 @@
 <?php
 require 'PHPMailerAutoload.php';
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 
 function sendEmail($toAddress, $fromAddress, $content, $subject, $user, $pass, $output){
-	$mail = new PHPMailer(); // create a new object
-	$mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-	$mail->do_debug = 0;
-	$mail->SMTPAuth = true; // authentication enabled
-	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-	$mail->Host = "smtp.zoho.com";
-	$mail->Port = 465; // or 587    
-	$mail->IsHTML(true);
-	$mail->Username = "$user";
-	$mail->Password = "$pass";
-	$mail->SetFrom($fromAddress, 'MRW Creations');
-	$mail->AddReplyTo('info@mrwcreations.com', 'MRW Creations');
-	$mail->Subject = $subject;
-	$mail->Body = $content;
-	$mail->AddAddress($toAddress);
+	try{
+		$mail = new PHPMailer(true); // create a new object
+		$mail->IsSMTP(); // enable SMTP
+		$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+		$mail->do_debug = 0;
+		$mail->SMTPAuth = true; // authentication enabled
+		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+		$mail->Host = "smtp.zoho.com";
+		$mail->Port = 465; // or 587    
+		$mail->IsHTML(true);
+		$mail->Username = "$user";
+		$mail->Password = "$pass";
+		$mail->SetFrom($fromAddress, 'MRW Creations');
+		$mail->AddReplyTo('info@mrwcreations.com', 'MRW Creations');
+		$mail->AddCC('info@mrwcreations.com', 'MRW Creations');
+		$mail->Subject = $subject;
+		$mail->Body = $content;
+		$mail->AddAddress($toAddress);
 
-	if(!$mail->Send() && $output) {
-		return '{"status": false, "message": "Unsuccessful"}';
-	} else if($output) {
-		return '{"status": true, "message": "Successful"}';
-	}	
+		if(!$mail->Send() && $output) {
+			return '{"status": false, "message": "'.$output.'"}';
+		} else if($output) {
+			return '{"status": true, "message": "Successful"}';
+		}	
+	}
+	catch(phpmailerException $e){
+		echo $e->errorMessage();
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+	}
 }
 ?>
