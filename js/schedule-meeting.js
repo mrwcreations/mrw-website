bcc1a74ceb6d8328cc2e8e48be843ba06ae6f5ba
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-10-30 12:53:02 -0500

    Adding invisible captcha on schedule meeting page

--- js/schedule-meeting.js
+++ js/schedule-meeting.js
@@ -6,7 +6,7 @@ $(".contact-form").on('change', function(){
 	} else if($('input[name=meeting-option]:checked', '.contact-form').val() === "meeting-person"){
 		$('.address').show();
 		$('.phone').hide();
-		$('.email').hide();
+		$('.email').show();
 	}
 });
 $('.form_datetime').datetimepicker({
@@ -26,21 +26,21 @@ var autocomplete = new google.maps.places.Autocomplete($("#input-address")[0], {
 
 google.maps.event.addListener(autocomplete, 'place_changed', function() {
     var place = autocomplete.getPlace();
-    console.log(place.address_components);
-});
-$(".submit").on('click', function(e){
-	e.preventDefault();
+});	
+
+function onRecaptcha(){
+	grecaptcha.execute();
+	$("form").on('submit', onSubmit(event));
+}
+$("form").on('submit', onSubmit(event));
+$(".submit").on('click', onSubmit(event));
+
+function onSubmit(e){
+	// e.preventDefault();
 	var phone = "+1 (123) 456-7890", email="noemail@provided.com", address="No address provided";
 	var meeting_time, meeting_description;
 	if($(".meeting-phone").is(":checked")){
 		phone = $("#input-phone").val() ? $("#input-phone").val() : phone;
-		email = $("#input-email").val();
-		if(email.length === 0){
-			$(".email #error").show();
-			return false;
-		}else{
-			$(".email #error").hide();
-		}
 	}
 	else if($(".meeting-person").is(":checked")){
 		address = $("#input-address").val();
@@ -50,22 +50,34 @@ $(".submit").on('click', function(e){
 			$(".address #error").hide();
 		}
 	}
+	email = $("#input-email").val();
+	if(email.length === 0){
+		$(".email #error").show();
+		return false;
+	}else{
+		$(".email #error").hide();
+	}
 	meeting_time = $("#dtp_input1").val(); 
 	name = $("#meeting-name").val();
 	meeting_description = $("textarea#meeting-description").val();
 	if(validate(phone, meeting_time, email, address, name)){
+		$("input[type=submit]").attr("disabled", "disabled");
+		$(".submit").html("Submitting...");
 		$.post(
-			"/scripts/form_submit.php",
+			"https://www.amitnandanp.com/scripts/form_submit.php",
 			{
 				name: name,
 				meeting_time: meeting_time,
 				meeting_description: meeting_description,	
 				email: email,
 				phone: phone,
-				address: address
+				address: address,
+				captcha_response: grecaptcha.getResponse()
 			},
 			function(data){
 				data = JSON.parse(data);
+				$("input[type=submit]").removeAttr("disabled");
+				$(".submit").html("Submit");
 				if(data.message === "Successful"){
 					$(".client_name").html(name);
 					$(".form-submit-successful").show();
@@ -73,13 +85,14 @@ $(".submit").on('click', function(e){
 					$(".contact-form").find("input textarea").val("");
 				}else{
 					$("#myModal").modal('show');
+					$(".form-submit-failure").html(data.message);
 					$(".form-submit-failure").fadeIn(1000);
 				}
 
 			}
 		);
     }
-});
+}
 function validate(phone, meeting_time, email, address, name){
 	var email_regex  = /^[a-z][a-zA-Z0-9_]*(\.[a-zA-Z][a-zA-Z0-9_]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
 	var phone_regex  = /^(\+)1{1}(\ \()[0-9]{3}(\) )[0-9]{3}(-)[0-9]{4}?$/;
--- scripts/form_submit.php
+++ scripts/form_submit.php
@@ -1,6 +1,7 @@
 <?php
-
 require('mail/sendmail.php');
+require('recaptcha.php');
+header("Access-Control-Allow-Origin: *");
 date_default_timezone_set("America/Chicago");
 $curl = curl_init();
 $ini_array = parse_ini_file("keys.ini");
@@ -12,6 +13,38 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $address = $_POST["address"];
 $apikey = $ini_array["key"];
+$headers = array(
+  "cache-control: no-cache",
+  "content-type: application/x-www-form-urlencoded",
+  "x-apikey: ".$apikey
+);
+
+$recaptcha = $_POST['captcha_response'];
+// $recaptcha_object = new Recaptcha();
+
+// $captcha_response = $recaptcha_object->verifyResponse($recaptcha);
+
+// function sendPosrRequest($postFields, $url, $headers){
+//   curl_setopt_array($curl, array(
+//     CURLOPT_URL => $url,
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => "",
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 30,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => "POST",
+//     CURLOPT_POSTFIELDS => $postFields,
+//     CURLOPT_HTTPHEADER => $headers,
+//   ));
+//   $response = curl_exec($curl);
+//   $err = curl_error($curl);
+//   if($err){
+//     return $err;
+//   }
+//   else {
+//     return $response;
+//   }
+// }
 
 $postFields = 'name-or-org-name='.$name.'&email='.$email.'&phone='.$phone.'&meeting-datetime='.$meeting_datetime->format('DD/MM/YY hh:mm').'&address='.$address.'&product-description='.$meeting_description;
 $postFields = str_replace(' ', '%20', $postFields);
@@ -37,6 +70,7 @@ $response = curl_exec($curl);
 $err = curl_error($curl);
 
 error_log($response);
+error_log($_SERVER['HTTP_HOST']);
 curl_close($curl);
 
 $meeting_time = $meeting_datetime->format('h:ma');
@@ -52,10 +86,15 @@ $subject = "Thank you ".$name."! Your meeting is set up!";
 $email_body = str_replace("{NAME}", $name, $email_body);
 $email_body = str_replace("{DATE}", $meeting_date, $email_body);
 $email_body = str_replace("{TIME}", $meeting_time, $email_body);
+if($address == "No address provided"){
+  $email_body = str_replace("{PLACE}", "", $email_body);
+} else {
+  $email_body = str_replace("{PLACE}", "at ".$address, $email_body);
+}
+$email_body = str_replace("{LINK}", "http://".$_SERVER['HTTP_HOST']."/mailer/meeting", $email_body);
 
-
-if ($err) {
-  echo "cURL Error #:" . $err;
+if(isset($captcha_response['success']) && $captcha_response['success'] != true) {
+  return '{"status": false, "message": "<strong>Oops!</strong> Seems like we were not able to verify your Google Recaptcha Identiy. Please refresh the page and try again"}';
 } else {
   echo sendEmail($email, $username, $email_body, $subject, $username, $password, true);
 }
