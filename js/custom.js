2afe64d07b7f9e017adca863404a27e3bcc0b976
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-10-30 18:50:20 -0500

    Changing page title on change of location hash #33

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,22 @@
 "use strict";
 
+var navigation = {
+  step: [
+    {
+      hash: "#homepage",
+      title: "Home Page >> MRW Creations - Artistry, Design and Development"
+    },
+    {
+      hash: "#about_us",
+      title: "About Us >>MRW Creations - Artistry, Design and Development"
+    },
+    {
+      hash: "#schedule_meeting",
+      title: "Schedule Meeting >> MRW Creations - Artistry, Design and Development"
+    }
+  ]
+};
+
 var step1 = "#homepage";
 var step2 = "#about_us";
 var step3 = "#schedule_meeting";
@@ -90,29 +107,36 @@ $(document).ready(function(){
   $carousel.bind('slide.bs.carousel', function(e){
     var childsList = Array.prototype.slice.call( $('.carousel').children );
     if(e.relatedTarget.outerHTML.indexOf("step_1") > 0){
-      window.location.hash = step1;
+      window.location.hash = navigation.step[0].hash;
+      document.title = navigation.step[0].title;
     } else if(e.relatedTarget.outerHTML.indexOf("step_2") > 0){
-      window.location.hash = step2;
+      window.location.hash = navigation.step[1].hash;
+      document.title = navigation.step[1].title;
     } else if(e.relatedTarget.outerHTML.indexOf("step_3")){
-      window.location.hash = step3;
+      window.location.hash = navigation.step[2].hash;
+      document.title = navigation.step[2].title;
     }
   });
   function hashChange(input_hash){
     switch(input_hash){
-      case step1:
+      case navigation.step[0].hash:
         pushAnalytics();
         $carousel.carousel(0);
+        document.title = navigation.step[0].title;
         break;
-      case step2:
+      case navigation.step[1].hash:
         pushAnalytics();
         $carousel.carousel(1);
+        document.title = navigation.step[1].title;
         break;
-      case step3:
+      case navigation.step[2].hash:
         pushAnalytics();
         $carousel.carousel(2);
+        document.title = navigation.step[2].title;
         break;
       default:
         $carousel.carousel(0);
+        document.title = navigation.step[3].title;
     }
   }
   function pushAnalytics(){
