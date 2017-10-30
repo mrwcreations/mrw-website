b9710d0615c5892ea4d350e9fe23e2a3134ca57f
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-10-30 12:57:03 -0500

    Adding description and other metadata for SEO optimization

--- js/scroll-events.js
+++ js/scroll-events.js
@@ -4,7 +4,6 @@ if (window.attachEvent) //if IE (and Opera depending on user setting)
     window.attachEvent("on"+mousewheelevt, function(e){parent.scrollEvent(e)})
 else if (window.addEventListener) //WC3 browsers
     window.addEventListener(mousewheelevt, function(e){parent.scrollEvent(e)}, false)
-console.log(document.location);
 // $(window).on("on"+mousewheelevt, parent.scrollEvent);
 $(window).on('touchstart', parent.touchStart);
 $(window).on('touchend', parent.touchEnd);
